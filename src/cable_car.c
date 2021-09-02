2b37f38449f5917f6850a9ce03fdcd458185a64d
bleakassassin <znelson.93shift@gmail.com>
2021-09-01 23:52:23 -0500

    Show proper outfit for Cable Car cinematic

--- src/cable_car.c
+++ src/cable_car.c
@@ -799,9 +799,9 @@ static void CreateCableCarSprites(void)
     u8 spriteId;
     u8 i;
 
-    u16 playerGraphicsIds[2] = {
-        [MALE]   = OBJ_EVENT_GFX_RIVAL_BRENDAN_NORMAL,
-        [FEMALE] = OBJ_EVENT_GFX_RIVAL_MAY_NORMAL
+    u16 playerGraphicsIds[OUTFIT_TOTAL][2] = {
+        [OUTFIT_EMERALD]   = {OBJ_EVENT_GFX_RIVAL_BRENDAN_NORMAL, OBJ_EVENT_GFX_RIVAL_MAY_NORMAL},
+        [OUTFIT_RS] = {OBJ_EVENT_GFX_LINK_RS_BRENDAN, OBJ_EVENT_GFX_LINK_RS_MAY},
     };
     u16 rval = Random();
     u16 hikerGraphicsIds[4] = {
@@ -825,7 +825,7 @@ static void CreateCableCarSprites(void)
         case FALSE:
         default:
             // Create player sprite
-            spriteId = AddPseudoObjectEvent(playerGraphicsIds[gSaveBlock2Ptr->playerGender], SpriteCB_Player, 200, 73, 102);
+            spriteId = AddPseudoObjectEvent(playerGraphicsIds[gSaveBlock2Ptr->costumeId][gSaveBlock2Ptr->playerGender], SpriteCB_Player, 200, 73, 102);
             if (spriteId != MAX_SPRITES)
             {
                 gSprites[spriteId].oam.priority = 2;
@@ -853,7 +853,7 @@ static void CreateCableCarSprites(void)
         case TRUE:
             CopyToBgTilemapBufferRect_ChangePalette(0, sCableCar->groundTilemap + 0x24, 24, 26, 12, 3, 17);
             // Create player sprite
-            spriteId = AddPseudoObjectEvent(playerGraphicsIds[gSaveBlock2Ptr->playerGender], SpriteCB_Player, 128, 39, 102);
+            spriteId = AddPseudoObjectEvent(playerGraphicsIds[gSaveBlock2Ptr->costumeId][gSaveBlock2Ptr->playerGender], SpriteCB_Player, 128, 39, 102);
             if (spriteId != MAX_SPRITES)
             {
                 gSprites[spriteId].oam.priority = 2;
