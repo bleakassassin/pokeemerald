827ebb64ba6fe4ec43d55e4c5c7261dfbd442f36
bleakassassin <znelson.93shift@gmail.com>
2021-09-02 02:10:06 -0500

    Fixed player credits graphics

--- src/intro_credits_graphics.c
+++ src/intro_credits_graphics.c
@@ -1220,16 +1220,16 @@ u8 CreateIntroBrendanSprite(s16 x, s16 y)
     u8 playerSpriteId;
     u8 bicycleSpriteId;
 	
-	if ((gSaveFileStatus != SAVE_STATUS_EMPTY && gSaveBlock2Ptr->costumeId == OUTFIT_RS)
+	if ((gSaveFileStatus != SAVE_STATUS_EMPTY && gSaveBlock2Ptr->costumeId == OUTFIT_EMERALD)
 		|| (x == DISPLAY_WIDTH + 32 && y == 46))
-	{
-	    playerSpriteId = CreateSprite(&sSpriteTemplate_RSBrendan, x, y, 2);
-	    bicycleSpriteId = CreateSprite(&sSpriteTemplate_RSBrendanBicycle, x, y + 8, 3);
-	}
-	else
 	{
 	    playerSpriteId = CreateSprite(&sSpriteTemplate_Brendan, x, y, 2);
 	    bicycleSpriteId = CreateSprite(&sSpriteTemplate_BrendanBicycle, x, y + 8, 3);
+	}
+	else
+	{
+	    playerSpriteId = CreateSprite(&sSpriteTemplate_RSBrendan, x, y, 2);
+	    bicycleSpriteId = CreateSprite(&sSpriteTemplate_RSBrendanBicycle, x, y + 8, 3);
 	}
     gSprites[bicycleSpriteId].sPlayerSpriteId = playerSpriteId;
     return playerSpriteId;
@@ -1240,16 +1240,16 @@ u8 CreateIntroMaySprite(s16 x, s16 y)
     u8 playerSpriteId;
     u8 bicycleSpriteId;
 	
-	if ((gSaveFileStatus != SAVE_STATUS_EMPTY && gSaveBlock2Ptr->costumeId == OUTFIT_RS)
+	if ((gSaveFileStatus != SAVE_STATUS_EMPTY && gSaveBlock2Ptr->costumeId == OUTFIT_EMERALD)
 		|| (x == DISPLAY_WIDTH + 32 && y == 46))
-	{
-	    playerSpriteId = CreateSprite(&sSpriteTemplate_RSMay, x, y, 2);
-        bicycleSpriteId = CreateSprite(&sSpriteTemplate_RSMayBicycle, x, y + 8, 3);
-	}
-	else
 	{
 	    playerSpriteId = CreateSprite(&sSpriteTemplate_May, x, y, 2);
         bicycleSpriteId = CreateSprite(&sSpriteTemplate_MayBicycle, x, y + 8, 3);
+	}
+	else
+	{
+	    playerSpriteId = CreateSprite(&sSpriteTemplate_RSMay, x, y, 2);
+        bicycleSpriteId = CreateSprite(&sSpriteTemplate_RSMayBicycle, x, y + 8, 3);
 	}
     gSprites[bicycleSpriteId].sPlayerSpriteId = playerSpriteId;
     return playerSpriteId;
