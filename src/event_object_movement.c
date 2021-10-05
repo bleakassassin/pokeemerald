dfecc68737e91f5713ed68c748837194ff58e50f
bleakassassin <znelson.93shift@gmail.com>
2021-10-04 23:36:40 -0500

    Add, adjust Fly locations for water Routes
    *Change shallow water tile behavior to not activate for player while marked invisible (i.e. when dismounting after Flying).
    *Enable Flying to Route 108 in front of Abandoned Ship after you board it.
    *Revert map changes to Routes 128 and 134; not needed after shallow water Flying fix.
    *Moved Route 125 Fly destination to in front of Shoal Cave.

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -7585,6 +7585,8 @@ static void GetGroundEffectFlags_ShallowFlowingWater(struct ObjectEvent *objEven
             objEvent->inShallowFlowingWater = 1;
             *flags |= GROUND_EFFECT_FLAG_SHALLOW_FLOWING_WATER;
         }
+		if (gObjectEvents[gPlayerAvatar.objectEventId].invisible)
+			gObjectEvents[gPlayerAvatar.objectEventId].inShallowFlowingWater = 0;
     }
     else
     {
